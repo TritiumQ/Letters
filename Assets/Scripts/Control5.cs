f64e7757f3af73120ad4555ab03dac65faece853
TtrtiumQ <qun02112@126.com>
2022-10-26 19:47:01 +0800

    资源与场景更新

--- Assets/Scripts/Control5.cs
+++ Assets/Scripts/Control5.cs
@@ -1,22 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Hospital : MonoBehaviour
+public class Control5 : MonoBehaviour
 {
-    public Image BG;
-    public DialogueData_SO DS1,DS2;
+    public DialogueData_SO D1, D2;
     private int num = 1;
     private void Awake()
     {
-        BG.gameObject.SetActive(false);
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        DialogueUI.Instance.UpdateDialogue(DS1);
-        DialogueUI.Instance.UpdateMainDialogue(DS1.dialoguePieces[0]);
+        DialogueUI.Instance.UpdateDialogue(D1);
+        DialogueUI.Instance.UpdateMainDialogue(D1.dialoguePieces[0]);
     }
 
     // Update is called once per frame
@@ -27,19 +25,16 @@ public class Hospital : MonoBehaviour
             switch (num)
             {
                 case 1:
-                    BG.gameObject.SetActive(true);
-                    DialogueUI.Instance.UpdateDialogue(DS2);
-                    DialogueUI.Instance.UpdateMainDialogue(DS2.dialoguePieces[0]);
+                    DialogueUI.Instance.UpdateDialogue(D2);
+                    DialogueUI.Instance.UpdateMainDialogue(D2.dialoguePieces[0]);
                     num++;
                     break;
                 case 2:
                     Debug.Log("TODO-结束");
                     num++;
                     break;
+              
             }
-            
         }
     }
-
-
 }
